a63db46706132b65fa6ad04fdaf3f0c239ada6db
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2024-09-22 00:06:25 -0600

    origen de chiapa de corzo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { PatrimonioComponent } from './pages/patrimonio/patrimonio.component';
 
-export const routes: Routes = [
-    { path: 'patrimonio', component: PatrimonioComponent },
-    { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-];
+export const routes: Routes = [];
